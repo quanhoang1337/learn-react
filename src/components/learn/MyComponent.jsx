19371915279ff8fb17ff503908941a7c4660b713
quanhoang1337 <quanhoang1337@gmail.com>
2026-01-02 09:41:16 +0700

    vid 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,16 @@
+import './style.css';
+
+
+
+
+
+
 const MyComponent = () => {
     return (
-        <div >MyComponent!</div>
+        <>
+            <div >MyComponent!</div>
+            <div className='child'>CHILD</div>
+        </>
     );
 }
 
