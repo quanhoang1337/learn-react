da70ac9547608cd63b520bed2e2f8bdaa124c0ee
quanhoang1337 <quanhoang1337@gmail.com>
2026-01-04 10:07:54 +0700

    vid 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,14 +2,20 @@ import './style.css';
 
 
 
-
-
-
 const MyComponent = () => {
+    const quanhoang = "quan"
+    const bool = undefined
+    const arr = {
+        name : "quan",
+        age : 18
+    }
     return (
         <>
-            <div >MyComponent!</div>
-            <div className='child'>CHILD</div>
+            <div>{JSON.stringify(arr)} MyComponent!</div>
+            <div>{console.log("ERIC")}</div>
+            <div className='child'
+                style={{borderRadius: "10px"}}
+            >CHILD</div>
         </>
     );
 }
