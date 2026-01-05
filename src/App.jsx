5bafd80ede0761059c13d80c772b995fc1ad5db6
quanhoang1337 <quanhoang1337@gmail.com>
2026-01-05 14:18:22 +0700

    vid 33

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,21 @@
-import { useState } from 'react'
+import "./components/todo/todo.css"
+import TodoData from "./components/todo/TodoData"
+import TodoNew from "./components/todo/TodoNew"
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from './components/learn/MyComponent'
-import {SecondComponent, ThirdComponent} from "./components/learn/SecondComponent "
-
-
-
-
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="todo-container">
+      <div className="todo-title">Todo list</div>
+      <TodoNew />
+      <TodoData />
+      <div className="todo-image">
+        <img src={reactLogo} className="logo" />
       </div>
-      <h1>Hoàng Quân</h1>
-      <MyComponent></MyComponent>
-      <SecondComponent></SecondComponent>
-      <ThirdComponent></ThirdComponent>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
+
 }
 
 export default App
