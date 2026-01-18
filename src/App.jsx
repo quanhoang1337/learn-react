86492e86c3a876529cccaffc31239694e8934e02
quanhoang1337 <quanhoang1337@gmail.com>
2026-01-18 10:22:53 +0700

    vid 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from "./components/todo/TodoData"
 import TodoNew from "./components/todo/TodoNew"
 import reactLogo from './assets/react.svg'
 import { useState } from "react"
+import Header from "./components/layout/header"
+import Footer from "./components/layout/footer"
 
 const App = () => {
 
@@ -31,24 +33,26 @@ const App = () => {
 
 
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo list</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
-
-      {todoList.length > 0 ?
-        <TodoData
-          todoList={todoList}
-          deleteTodo={deleteTodo}
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo list</div>
+        <TodoNew
+          addNewTodo={addNewTodo}
         />
-        :
-        <div className="todo-image">
-          <img src={reactLogo} className="logo" />
-        </div>
-      }
 
-      {/* {todoList.length > 0 &&
+        {todoList.length > 0 ?
+          <TodoData
+            todoList={todoList}
+            deleteTodo={deleteTodo}
+          />
+          :
+          <div className="todo-image">
+            <img src={reactLogo} className="logo" />
+          </div>
+        }
+
+        {/* {todoList.length > 0 &&
         <TodoData
           todoList={todoList}
         />
@@ -60,7 +64,9 @@ const App = () => {
           <img src={reactLogo} className="logo" />
         </div>
       } */}
-    </div>
+      </div>
+      <Footer />
+    </>
   )
 
 }
