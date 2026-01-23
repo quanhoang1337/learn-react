2322de9ef917bef6f4e711fc6ce2c57599cd0b27
quanhoang1337 <quanhoang1337@gmail.com>
2026-01-23 09:51:01 +0700

    vid 69

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,5 @@
 import { Button, Input } from "antd";
+import axios from "axios";
 import { useState } from "react";
 
 
@@ -10,7 +11,15 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
     const handleClickBtn = () => {
-        console.log("check state ", {fullName, email, password, phone})
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone
+        }
+        axios.post(URL_BACKEND, data)
+        console.log("check state ", { fullName, email, password, phone })
     }
 
 
